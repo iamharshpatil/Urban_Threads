854cbe6624aac4242430d75cfb31a148fbe92f95
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-08-29 19:12:57 +0530

    cors added

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -10,7 +10,7 @@ export const addNewProduct = createAsyncThunk(
   "/products/addnewproduct",
   async (formData) => {
     const result = await axios.post(
-      "http://localhost:5000/api/admin/products/add",
+      "https://urban-threads-kjrr.onrender.com/api/admin/products/add",
       formData,
       {
         headers: {
