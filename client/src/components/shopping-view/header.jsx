0b9d6af5225ad16aca91a8704c6fe20349b16f6f
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-08-30 13:30:35 +0530

    F: Logo updated

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -23,6 +23,8 @@ import UserCartWrapper from "./cart-wrapper";
 import { useEffect, useState } from "react";
 import { fetchCartItems } from "@/store/shop/cart-slice";
 import { Label } from "../ui/label";
+import Logo from '/public/Logo.png'
+
 
 function MenuItems() {
   const navigate = useNavigate();
@@ -139,8 +141,7 @@ function ShoppingHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/shop/home" className="flex items-center gap-2">
-          <HousePlug className="h-6 w-6" />
-          <span className="font-bold">Ecommerce</span>
+                  <img className="w-20" src={Logo} alt="Urban Threads Logo" />
         </Link>
         <Sheet>
           <SheetTrigger asChild>
