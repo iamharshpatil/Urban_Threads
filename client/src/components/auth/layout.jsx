9578fb960fdb34aeb42326cb5c6adfa118ee26f4
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-08-31 13:29:15 +0530

    login ui

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router-dom";
 
+
 function AuthLayout() {
   return (
     <div className="flex min-h-screen w-full">
-      <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
-        <div className="max-w-md space-y-6 text-center text-primary-foreground">
-          <h1 className="text-4xl font-extrabold tracking-tight">
-            Welcome to ECommerce Shopping
-          </h1>
-        </div>
+      <div className="hidden lg:flex items-center bg-[#FAF3E0] justify-center  w-1/2 px-12">
+    
       </div>
       <div className="flex flex-1 items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
         <Outlet />
@@ -18,3 +15,4 @@ function AuthLayout() {
 }
 
 export default AuthLayout;
+       
