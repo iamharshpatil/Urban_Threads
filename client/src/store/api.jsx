dc770b100cc45e61ca7b112f4e0fdd56fd8a1bc1
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-09-01 00:28:21 +0530

    ui updated v1

--- client/src/store/api.jsx
+++ client/src/store/api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://urban-threads-kjrr.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
